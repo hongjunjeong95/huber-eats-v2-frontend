c8ca6ae84c3c131195761b20a643c3560399117e
zpskek <wjdghdwns0@gmail.com>
2021-07-24 10:18:04 +0900

    Set up tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="bg-red-500">hello</div>
+    </div>
+  );
 }
 
 export default App;
