f55e69307e94deb7fb14a91d024f25dcf757e3f3
zpskek <wjdghdwns0@gmail.com>
2021-07-28 12:15:41 +0900

    Install react/fontawesome

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
 import cooGetherLogo from "../images/logo.png";
 
 export const Header = () => {
@@ -10,8 +13,10 @@ export const Header = () => {
           <img src={cooGetherLogo} alt="Logo" className="w-28" />
         </Link>
         <div className="flex items-center">
-          <button className="mr-5 text-lg font-bold">Logout</button>
-          <Link to="#">Edit Profile</Link>
+          <button className="mr-5 text-2xl   font-bold">Logout</button>
+          <Link to="#">
+            <FontAwesomeIcon icon={faUser} className="text-2xl" />
+          </Link>
         </div>
       </div>
     </header>
